f856a3b4703eaf8015d59e58f45f9c84bf90825a
Tay Kong Yu <kongyutay@gmail.com>
2025-01-05 14:54:56 +0800

    highest rated and popular books query

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -17,4 +17,14 @@ class Book extends Model
     public function scopeTitle(Builder $query, string $title): Builder {
         return $query->where("title","like","%". $title ."%");
     }
+
+    // 作用域方法定义的时候要加scope前缀，但是laravel会自动创建popular方法这是一个命名约定，例如\App\Models\Book::popular()->highestRated()->get()
+    // 调用的时候不用给popular传递参数因为已经把Builder这个query构造器传入（依赖注入）了，laravel会自动处理
+    public function scopePopular(Builder $query): Builder {
+        return $query->withCount('reviews')->orderBy('reviews_count','desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
 }
