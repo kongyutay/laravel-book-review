ebd3208ac6a320f304e999ac40508ccd1989233d
Tay Kong Yu <kongyutay@gmail.com>
2025-01-06 22:15:48 +0800

    controller - index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 绑定了之后会自动create相关路由
+Route::resource('books', BookController::class);
